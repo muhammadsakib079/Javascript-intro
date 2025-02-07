5dc0cb3e35a12cb52d9eaa11b00445610a893b31
Md Sakib <muhammadsakib@4l68v8.onmicrosoft.com>
2025-02-07 20:59:56 +0600

    push--add

--- if_else.js
+++ if_else.js
@@ -1,37 +1,37 @@
 // Enter the Unity of Fruit
 
-let apples = 40;
-let oranges = 12; 
-let goabas = 10;
+// let apples = 40;
+// let oranges = 12; 
+// let goabas = 10;
+
+let Unity = [40, 12, 10];
+
 
 // Get Discount
-let love = true; 
-
-
-
-
-
-
-
-
-
-
-
-
+let love = false; 
 
 
 // Product Price 
-let applePerKg = 390;
-let orangePerKg = 330;
-let goabaPerKg = 250;
+// let applePerKg = 390;
+// let orangePerKg = 330;
+// let goabaPerKg = 250;
+
+const fruits = [390, 330, 250];
+
+
 // // Dicount Cupon
 let EMA = 12 / 100;
 let SAKIB = 8 / 100;
 // Discount Value
-if(love == true){cupon = EMA}
-else{cupon = SAKIB};
+
+// if(love == true){cupon = EMA}
+// else{cupon = SAKIB};
+
+cupon = love ? EMA : SAKIB;
+
+
 // Price Counting Process
-let grossPrice = ((applePerKg * apples) + (orangePerKg * oranges) + (goabaPerKg * goabas));
+let grossPrice = (fruits[0] * Unity[0]) + (fruits[1] * Unity[1]) + (fruits[2] * Unity[2]);
 let discount = ((grossPrice * cupon).toFixed(0));
 let totalPrice = (grossPrice - discount);
 console.log("Gross Price:- " + grossPrice);
---
+++ push.js
@@ -0,0 +1,4 @@
+let Unity = [40, 12];
+Unity.push(400);
+Unity.push(45000);
+console.log(Unity);
